7aa7a17b0ea645d8011987da01bed98f6a296ab0
Dipam Sen <funplanet.m4f@gmail.com>
2023-01-01 13:02:45 +0530

    Update p5.interactivity.js

--- p5.interactivity.js
+++ p5.interactivity.js
@@ -1,31 +1,44 @@
-
-let ZOOM_MAX = 3
-let ZOOM_MIN = 1/3
-
 p5.prototype._initVals = function () {
   this.int_zoom = 1;
   this.int_offset = this.createVector(0, 0);
   
-  this.mouseWheel = mouseWheel1.bind(this)
-  this.mouseDragged = mouseDragged1.bind(this)
-  
+  this.ZOOM_LIM = [1/3, 3]
+
+  this.mouseWheel = mouseWheel1.bind(this);
+  this.mouseDragged = mouseDragged1.bind(this);
 };
 
-p5.prototype.registerMethod('init', p5.prototype._initVals)
+p5.prototype.registerMethod("init", p5.prototype._initVals);
 
 p5.prototype.interactive = function () {
-  scale(this.int_zoom);
-  translate(this.int_offset);
+  const ctx = this._isGlobal ? window : this;
+
+  let { e, f } = ctx.drawingContext.getTransform();
+  origin = { x: e, y: f };
+
+  scale(ctx.int_zoom);
+  translate(ctx.int_offset);
 };
 
-
 function mouseWheel1(event) {
+  const ctx = this._isGlobal ? window : this;
   // convert delta value into zoom
-  this.int_zoom *= 2 ** (-event.delta / 50);
-  this.int_zoom = constrain(this.int_zoom, ZOOM_MIN, ZOOM_MAX);
+  ctx.int_zoom *= 2 ** (-event.delta / 50);
+  ctx.int_zoom = constrain(ctx.int_zoom, ...ctx.ZOOM_LIM);
   return false;
 }
 
 function mouseDragged1(event) {
-  this.int_offset.add(event.movementX / this.int_zoom, event.movementY / this.int_zoom);
+  const ctx = this._isGlobal ? window : this;
+
+  ctx.int_offset.add(
+    event.movementX / ctx.int_zoom,
+    event.movementY / ctx.int_zoom
+  );
 }
+
+p5.prototype.getViewport = function () {
+  const ctx = this._isGlobal ? window : this;
+
+  return { x: -ctx.int_offset.x - origin.x / ctx.int_zoom, y: -ctx.int_offset.y - origin.y / ctx.int_zoom, width: ctx.width / ctx.int_zoom, height: ctx.height / ctx.int_zoom };
+};
